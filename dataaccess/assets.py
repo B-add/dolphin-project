ce3491a4f488828209f22ffe94d6bd7fcc3aed2e
Valentin Lamatte <valentin.lamatte@epita.fr>
2017-11-24 23:44:51 +0100

    Add simple assets WS

--- dataaccess/assets.py
+++ dataaccess/assets.py
@@ -1,10 +1,25 @@
 import requests
 
 URL = 'https://dolphin.jump-technology.com:3389/api/v1/'
+AUTH = ('epita_user_1', 'dolphin20412')
 
 
 def get_assets():
-    res = requests.get(URL + 'asset?columns=ASSET_DATABASE_ID&columns=LABEL&columns=TYPE&TYPE=PORTFOLIO',
-                       auth=('epita_user_1', 'dolphin20412'),
+    res = requests.get(URL + 'asset',
+                       auth=AUTH,
                        verify=False)
-    print(res.content)
+    return res.content
+
+
+def get_asset(id):
+    res = requests.get(URL + 'asset/' + str(id),
+                       auth=AUTH,
+                       verify=False)
+    return res.content
+
+
+def get_asset_attribute(id, attribute):
+    res = requests.get(URL + 'asset/' + str(id) + '/attribute/' + attribute,
+                       auth=AUTH,
+                       verify=False)
+    return res.content
--- test.py
+++ test.py
@@ -1,3 +1,8 @@
-from dataaccess.assets import get_assets
+from dataaccess.assets import get_assets, get_asset, get_asset_attribute
 
 get_assets()
+
+get_asset(220)
+
+
+get_asset_attribute(220, 'LIQUIDITY_ALGO')
