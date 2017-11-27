dbf5e710751cb5012e386ee9b1f1b6d38ca0e4d6
Valentin Lamatte <valentin.lamatte@epita.fr>
2017-11-27 03:29:52 +0100

    Add set portfolio func

--- test.py
+++ test.py
@@ -6,11 +6,12 @@ from dataaccess.assets import get_assets, get_asset, get_asset_attribute, \
 
 from funcs import get_best_sharpes_ids, get_covar_mat, optimize_assets
 
-from dataaccess.portfolios import get_portfolio, get_our_portfolio
+from dataaccess.portfolios import get_portfolio, get_our_portfolio, \
+    set_test_portfolio
 
 from dataaccess.ratios import get_ratios, compute_ratios
 
-# print(get_assets(full_response=False, columns=["MARKET_PLACE_CITY"]))
+print(get_asset(id=443, full_response=False, columns=["LAST_CLOSE_VALUE"], date="2011-12-31"))
 
 # print(get_asset(id=221, full_response=False, columns=["LABEL", "ASSET_DATABASE_ID"]))
 # #
@@ -22,7 +23,7 @@ from dataaccess.ratios import get_ratios, compute_ratios
 #
 # print(get_portfolio(221))
 
-# print(get_portfolio())
+print(get_portfolio())
 from funcs import get_covar, get_id_efficiency_volatility_tuple
 
 # print(get_id_efficiency_volatility_tuple(get_best_sharpes_ids()))
@@ -30,4 +31,7 @@ from funcs import get_covar, get_id_efficiency_volatility_tuple
 # print(id_eff_vol)
 # print(len(asset_ids))
 
-print(optimize_assets(2, 0))
+# print(optimize_assets(2, 0))
+
+set_test_portfolio([530, 446, 442, 389, 440, 505, 447, 419, 384, 466, 509, 478, 540, 370, 471, 393, 416, 441, 460, 443],
+                   [0.530, 0.46, 0.442, 0.389, 0.440, 0.505, .447, .419, .384, .466, .509, .478, .540, .370, .471, .393, .416, .441, .460, .443])
